844270324b7b5b21538917a91a85b316fd65ed7a
Tomas Landovsky <landovsky@gmail.com>
2025-09-25 16:39:09 +0200

    Check ability against resource or scope

--- lib/flexi_admin/components/resource/form_component.rb
+++ lib/flexi_admin/components/resource/form_component.rb
@@ -4,6 +4,8 @@ module FlexiAdmin::Components::Resource
   class FormComponent < FlexiAdmin::Components::BaseComponent
     include FlexiAdmin::Components::Resource::FormMixin
 
+    ResourceOrScopeNotDefinedError = Class.new(StandardError)
+
     attr_reader :resource
 
     def initialize(resource, disabled: true)
@@ -13,10 +15,19 @@ module FlexiAdmin::Components::Resource
 
     def disabled
       if defined?(CanCan)
-        !helpers.current_ability&.can?(:update, resource) || @disabled
+        raise ResourceOrScopeNotDefinedError, "Resource or scope is not defined" if resource.blank? && scope.blank?
+
+        resource_to_check = resource.presence || scope.singularize.humanize.constantize
+
+        !helpers.current_ability&.can?(:update, resource_to_check) || @disabled
       else
         @disabled
       end
+    rescue ResourceOrScopeNotDefinedError => e
+      BugTracker.notify(e, scope: scope)
+
+      # Let it pass, we need to fix missing scope or resource
+      false
     end
   end
 end
