e29c65bbeb03c67f1a13bc8b15eb9fa817310bf8
Tomas Landovsky <landovsky@gmail.com>
2025-10-03 11:56:31 +0200

    Add pagination options to config, change pagination in page

--- lib/flexi_admin/components/resources/pagination_component.rb
+++ lib/flexi_admin/components/resources/pagination_component.rb
@@ -2,7 +2,7 @@
 
 module FlexiAdmin::Components::Resources
   class PaginationComponent < FlexiAdmin::Components::BaseComponent
-    LIMIT = 10
+    MAX_PAGES_TO_SHOW_IN_PAGINATOR = 10
 
     include FlexiAdmin::Components::Helpers::ResourceHelper
     include FlexiAdmin::Components::Helpers::UrlHelper
@@ -10,6 +10,7 @@ module FlexiAdmin::Components::Resources
     attr_reader :context, :per_page, :page, :limited_pagination
 
     def initialize(context, per_page:, page:)
+      super()
       @context = context
       @resources = context.resources
       @parent = context.parent
@@ -17,27 +18,25 @@ module FlexiAdmin::Components::Resources
       @page = page&.to_i
     end
 
-    def render?
-      paginated_resources.total_pages > 1
-    end
-
     def limited_pages
-      return (1..paginated_resources.total_pages).to_a if paginated_resources.total_pages < LIMIT
+      if paginated_resources.total_pages < MAX_PAGES_TO_SHOW_IN_PAGINATOR
+        return (1..paginated_resources.total_pages).to_a
+      end
 
       max = paginated_resources.total_pages
 
-      mid_range_start = if page > (LIMIT / 2) && page < (max - (LIMIT / 2))
-                          page - (LIMIT / 2)
-                        elsif page < (LIMIT / 2)
+      mid_range_start = if page > (MAX_PAGES_TO_SHOW_IN_PAGINATOR / 2) && page < (max - (MAX_PAGES_TO_SHOW_IN_PAGINATOR / 2))
+                          page - (MAX_PAGES_TO_SHOW_IN_PAGINATOR / 2)
+                        elsif page < (MAX_PAGES_TO_SHOW_IN_PAGINATOR / 2)
                           1
                         else
-                          max - LIMIT
+                          max - MAX_PAGES_TO_SHOW_IN_PAGINATOR
                         end
 
-      mid_range_end = if page > (LIMIT / 2) && page < (max - (LIMIT / 2))
-                        page + (LIMIT / 2)
-                      elsif page < (LIMIT / 2)
-                        LIMIT
+      mid_range_end = if page > (MAX_PAGES_TO_SHOW_IN_PAGINATOR / 2) && page < (max - (MAX_PAGES_TO_SHOW_IN_PAGINATOR / 2))
+                        page + (MAX_PAGES_TO_SHOW_IN_PAGINATOR / 2)
+                      elsif page < (MAX_PAGES_TO_SHOW_IN_PAGINATOR / 2)
+                        MAX_PAGES_TO_SHOW_IN_PAGINATOR
                       else
                         max
                       end
@@ -47,14 +46,22 @@ module FlexiAdmin::Components::Resources
 
     def page_path(page_number)
       params = context.params
-                      .merge(page: page_number, per_page:, frame: context.scope)
+                      .merge(page: page_number, per_page: per_page, frame: context.scope)
+                      .to_params
+
+      resources_path(**params.merge)
+    end
+
+    def per_page_path(new_per_page)
+      params = context.params
+                      .merge(page: 1, per_page: new_per_page, frame: context.scope)
                       .to_params
 
       resources_path(**params.merge)
     end
 
     def paginated_resources
-      @resources.paginate(page:, per_page:)
+      @resources.paginate(page: page, per_page: per_page)
     end
 
     def total_pages
--- lib/flexi_admin/config.rb
+++ lib/flexi_admin/config.rb
@@ -2,11 +2,23 @@
 
 module FlexiAdmin
   class Config
+    PAGINATE_PER_OPTIONS = [12, 24, 48, 96].freeze
+    PAGINATE_PER_DEFAULT = 12
+
+    def self.paginate_per_options
+      PAGINATE_PER_OPTIONS
+    end
+
+    def self.paginate_per_default
+      PAGINATE_PER_DEFAULT
+    end
+
     class Store
-      attr_accessor :namespace, :module_namespace, :paginate_per
+      attr_accessor :namespace, :module_namespace, :paginate_per, :paginate_per_options
 
       def initialize
-        @paginate_per = 15
+        @paginate_per = PAGINATE_PER_DEFAULT
+        @paginate_per_options = PAGINATE_PER_OPTIONS
       end
     end
 
