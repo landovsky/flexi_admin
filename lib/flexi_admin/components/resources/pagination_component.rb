faca7ca34a258f9cfcaac0475632c8d4e238ad0b
Tomáš Landovský <landovsky@gmail.com>
2026-02-10 07:40:47 +0100

    Flatten class identation

--- lib/flexi_admin/components/resources/pagination_component.rb
+++ lib/flexi_admin/components/resources/pagination_component.rb
@@ -1,71 +1,69 @@
 # frozen_string_literal: true
 
-module FlexiAdmin::Components::Resources
-  class PaginationComponent < FlexiAdmin::Components::BaseComponent
-    MAX_PAGES_TO_SHOW_IN_PAGINATOR = 10
+class FlexiAdmin::Components::Resources::PaginationComponent < FlexiAdmin::Components::BaseComponent
+  MAX_PAGES_TO_SHOW_IN_PAGINATOR = 10
 
-    include FlexiAdmin::Components::Helpers::ResourceHelper
-    include FlexiAdmin::Components::Helpers::UrlHelper
+  include FlexiAdmin::Components::Helpers::ResourceHelper
+  include FlexiAdmin::Components::Helpers::UrlHelper
 
-    attr_reader :context, :per_page, :page, :limited_pagination
+  attr_reader :context, :per_page, :page, :limited_pagination
 
-    def initialize(context, per_page:, page:)
-      super()
-      @context = context
-      @resources = context.resources
-      @parent = context.parent
-      @per_page = per_page
-      @page = page&.to_i
+  def initialize(context, per_page:, page:)
+    super()
+    @context = context
+    @resources = context.resources
+    @parent = context.parent
+    @per_page = per_page
+    @page = page&.to_i
+  end
+
+  def limited_pages
+    if paginated_resources.total_pages < MAX_PAGES_TO_SHOW_IN_PAGINATOR
+      return (1..paginated_resources.total_pages).to_a
     end
 
-    def limited_pages
-      if paginated_resources.total_pages < MAX_PAGES_TO_SHOW_IN_PAGINATOR
-        return (1..paginated_resources.total_pages).to_a
-      end
+    max = paginated_resources.total_pages
 
-      max = paginated_resources.total_pages
-
-      mid_range_start = if page > (MAX_PAGES_TO_SHOW_IN_PAGINATOR / 2) && page < (max - (MAX_PAGES_TO_SHOW_IN_PAGINATOR / 2))
-                          page - (MAX_PAGES_TO_SHOW_IN_PAGINATOR / 2)
-                        elsif page < (MAX_PAGES_TO_SHOW_IN_PAGINATOR / 2)
-                          1
-                        else
-                          max - MAX_PAGES_TO_SHOW_IN_PAGINATOR
-                        end
-
-      mid_range_end = if page > (MAX_PAGES_TO_SHOW_IN_PAGINATOR / 2) && page < (max - (MAX_PAGES_TO_SHOW_IN_PAGINATOR / 2))
-                        page + (MAX_PAGES_TO_SHOW_IN_PAGINATOR / 2)
+    mid_range_start = if page > (MAX_PAGES_TO_SHOW_IN_PAGINATOR / 2) && page < (max - (MAX_PAGES_TO_SHOW_IN_PAGINATOR / 2))
+                        page - (MAX_PAGES_TO_SHOW_IN_PAGINATOR / 2)
                       elsif page < (MAX_PAGES_TO_SHOW_IN_PAGINATOR / 2)
-                        MAX_PAGES_TO_SHOW_IN_PAGINATOR
+                        1
                       else
-                        max
+                        max - MAX_PAGES_TO_SHOW_IN_PAGINATOR
                       end
 
-      [1, (mid_range_start..mid_range_end).to_a, max].flatten.compact.uniq
-    end
+    mid_range_end = if page > (MAX_PAGES_TO_SHOW_IN_PAGINATOR / 2) && page < (max - (MAX_PAGES_TO_SHOW_IN_PAGINATOR / 2))
+                      page + (MAX_PAGES_TO_SHOW_IN_PAGINATOR / 2)
+                    elsif page < (MAX_PAGES_TO_SHOW_IN_PAGINATOR / 2)
+                      MAX_PAGES_TO_SHOW_IN_PAGINATOR
+                    else
+                      max
+                    end
 
-    def page_path(page_number)
-      params = context.params
-                      .merge(page: page_number, per_page: per_page, frame: context.scope)
-                      .to_params
+    [1, (mid_range_start..mid_range_end).to_a, max].flatten.compact.uniq
+  end
 
-      resources_path(**params.merge)
-    end
+  def page_path(page_number)
+    params = context.params
+                    .merge(page: page_number, per_page: per_page, frame: context.scope)
+                    .to_params
 
-    def per_page_path(new_per_page)
-      params = context.params
-                      .merge(page: 1, per_page: new_per_page, frame: context.scope)
-                      .to_params
+    resources_path(**params.merge)
+  end
 
-      resources_path(**params.merge)
-    end
+  def per_page_path(new_per_page)
+    params = context.params
+                    .merge(page: 1, per_page: new_per_page, frame: context.scope)
+                    .to_params
 
-    def paginated_resources
-      @resources.paginate(page: page, per_page: per_page)
-    end
+    resources_path(**params.merge)
+  end
 
-    def total_pages
-      (resources.count / per_page.to_f).ceil
-    end
+  def paginated_resources
+    @resources.paginate(page: page, per_page: per_page)
+  end
+
+  def total_pages
+    (resources.count / per_page.to_f).ceil
   end
 end
--- lib/flexi_admin/components/shared/table/header_item_component.rb
+++ lib/flexi_admin/components/shared/table/header_item_component.rb
@@ -1,78 +1,76 @@
 # frozen_string_literal: true
 
-module FlexiAdmin::Components::Shared::Table
-  class HeaderItemComponent < FlexiAdmin::Components::BaseComponent
-    attr_reader :column, :attr, :justify, :width, :sort_by
+class FlexiAdmin::Components::Shared::Table::HeaderItemComponent < FlexiAdmin::Components::BaseComponent
+  attr_reader :column, :attr, :justify, :width, :sort_by
 
-    def initialize(column)
-      @column = column
-      @justify = column.options[:justify] || :start
-      @width = column.options[:width] || ""
-      @attr = column.attribute
-      @sort_by = column.sort_by
-      @label = column.label
+  def initialize(column)
+    @column = column
+    @justify = column.options[:justify] || :start
+    @width = column.options[:width] || ""
+    @attr = column.attribute
+    @sort_by = column.sort_by
+    @label = column.label
+  end
+
+  def label
+    @label || attr.to_s.humanize
+  end
+
+  def sort_path
+    if order.blank?
+      view_context.context.params.to_path(request.path)
+    else
+      view_context.context.params.merge({ sort: sort_by, order: }).to_path(request.path)
+    end
+  end
+
+  def merged_classes
+    [width_class, justify_class].join(" ")
+  end
+
+  def width_class
+    width.present? ? "col-#{width}" : "col"
+  end
+
+  def justify_class
+    case justify.to_sym
+    when :start
+      "justify-content-start"
+    when :end
+      "justify-content-end"
+    else
+      "justify-content-start"
+    end
+  end
+
+  def sort_icon
+    return "bi-arrow-up-square" if view_context.context.params[:sort].to_s != sort_by.to_s
+
+    case view_context.context.params[:order]
+    when "asc"
+      "bi-arrow-up-square-fill"
+    when "desc"
+      "bi-arrow-down-square-fill"
+    else
+      "bi-arrow-up-square"
+    end
+  end
+
+  def order
+    # Sorting by other column, so we need to reset the order
+    return "asc" if view_context.context.params[:sort].to_s != sort_by.to_s
+
+    order = case view_context.context.params[:order]
+    when "asc"
+      "desc"
+    when "desc"
+      "default"
+    when "default"
+      "asc"
+    else
+      "asc"
     end
 
-    def label
-      @label || attr.to_s.humanize
-    end
-
-    def sort_path
-      if order.blank?
-        view_context.context.params.to_path(request.path)
-      else
-        view_context.context.params.merge({ sort: sort_by, order: }).to_path(request.path)
-      end
-    end
-
-    def merged_classes
-      [width_class, justify_class].join(" ")
-    end
-
-    def width_class
-      width.present? ? "col-#{width}" : "col"
-    end
-
-    def justify_class
-      case justify.to_sym
-      when :start
-        "justify-content-start"
-      when :end
-        "justify-content-end"
-      else
-        "justify-content-start"
-      end
-    end
-
-    def sort_icon
-      return "bi-arrow-up-square" if view_context.context.params[:sort].to_s != sort_by.to_s
-
-      case view_context.context.params[:order]
-      when "asc"
-        "bi-arrow-up-square-fill"
-      when "desc"
-        "bi-arrow-down-square-fill"
-      else
-        "bi-arrow-up-square"
-      end
-    end
-
-    def order
-      # Sorting by other column, so we need to reset the order
-      return "asc" if view_context.context.params[:sort].to_s != sort_by.to_s
-
-      order = case view_context.context.params[:order]
-      when "asc"
-        "desc"
-      when "desc"
-        "default"
-      when "default"
-        "asc"
-      else
-        "asc"
-      end
-
-      order
-    end
+    order
   end
 end
